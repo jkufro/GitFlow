62950ac1faf8a7bb5431c46083f3a1203c3194e5
Justin Kufro <kufroj@gmail.com>
2018-08-29 13:54:56 -0400

    added hello world java program

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * Recitation 1 for 17-313
+ * Andrew ID: jkufro
+ *
+ * @author Justin Kufro
+ * @since 8/29/2018
+ */
+public class HelloWorld {
+    /**
+     * hello world program.
+     *
+     * @param args any number of command line arguments (unused)
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
